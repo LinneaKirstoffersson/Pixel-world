e610811cffc89d5ccb65aff9eb1deb394727aca3
LinneaKristoffersson <linnea.kristoffersson@elev.ga.lbs.se>
2026-02-14 18:23:18 +0100

    comments
    done with the whole code comenting

--- pixel_script.js
+++ pixel_script.js
@@ -8,14 +8,15 @@
 
 /*----- js for chosegame section----*/
 function showGame(sectionId) {      /*chat gpts script that dose work*/
-    // hide all sections
-    document.getElementById("choseGame").style.display = "none";
+    /*hides the other parts when the function is clicked*/
+    document.getElementById("choseGame").style.display = "none";       
     document.getElementById("convinentstore").style.display = "none";
     document.getElementById("Cybr").style.display = "none";
 
-    // show the selected section
+    /*opens the part you clicked */
     document.getElementById(sectionId).style.display = "block";
 
+    /*opens the one that is opend*/
     if (sectionId === "convinentstore") { /*chat gpt made clener version*/
         storeIndex = 1;
         storeShowSlides(storeIndex);
@@ -28,6 +29,7 @@ function showGame(sectionId) {      /*chat gpts script that dose work*/
 }
 
 /*---- js for store slide show ---  disclamer i did not program the slide show i found and copypasted from w3school i have read trough it and understand most of it*/
+/*opens store part*/
 let storeIndex = 1;
 storeShowSlides(storeIndex);
 
@@ -41,8 +43,8 @@ function storeCurrentSlide(n) {
   storeShowSlides(storeIndex = n);
 }
 
-function storeShowSlides(n) {
-  let i;
+function storeShowSlides(n) {   /* opens the n img and gets the dots. not sure exacly how the rest works */
+  let i;                
   let slides = document.getElementsByClassName("storeSlides");
   let dots = document.getElementsByClassName("storedot");
   if (n > slides.length) {storeIndex = 1}
@@ -59,7 +61,7 @@ function storeShowSlides(n) {
 
 
 /*---- js for cybr slide show---  disclamer i did not program the slide show i found and copypasted from w3school i have read trough it and understand most of it*/
-
+/*opens the cybr part*/
 let cybrIndex = 1;
 cybrShowSlides(cybrIndex);
 
@@ -73,7 +75,7 @@ function cybrCurrentSlide(n) {
   cybrShowSlides(cybrIndex = n);
 }
 
-function cybrShowSlides(n) {
+function cybrShowSlides(n) {     /* opens the n img and gets the dots. not sure exacly how the rest works */
   let i;
   let slides = document.getElementsByClassName("cybrSlides");
   let dots = document.getElementsByClassName("cybrdot");
